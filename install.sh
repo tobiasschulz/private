8aa7aede89d9e2e9dbeda66bee1e1921793fdba0
Tobias Schulz <tobias@freehal.net>
2013-01-26 18:49:25 +0100

    bugfixes

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-LC_ALL=C git pull 2>&1 | grep -v "Current branch master is up to date" | grep -v "Already up-to-date"
-
+cd $(dirname $0)
 export P=$(pwd)
 
 # .bashrc
@@ -20,7 +19,7 @@ cp -f $P/apt.conf /etc/apt/apt.conf.d/60user
 # setup update script
 
 echo '#!/bin/bash' > /usr/local/bin/pull-private
-echo 'sudo su -c "cd '$P'; ./install.sh"' >> /usr/local/bin/pull-private
+echo 'sudo su -c "'$P'/update.sh"' >> /usr/local/bin/pull-private
 chown root:root /usr/local/bin/pull-private
 chmod u=rwx,g=rx,o=rx /usr/local/bin/pull-private
 ln -f /usr/local/bin/pull-private /etc/cron.daily/pull-private
