e5af03ba27e705e28935fb96c0e49350543fb12e
Tobias Schulz <tobias@freehal.net>
2013-01-26 17:21:32 +0100

    cleanup

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git pull
+LC_ALL=C git pull 2>&1 | grep -v "Current branch master is up to date"
 
 export P=$(pwd)
 for u in /etc/skel /root /home/*
@@ -14,3 +14,4 @@ echo '#!/bin/bash' > /usr/local/bin/pull-private
 echo 'sudo su -c "cd '$P'; ./install.sh"' >> /usr/local/bin/pull-private
 chown root:root /usr/local/bin/pull-private
 chmod u=rwx,g=rx,o=rx /usr/local/bin/pull-private
+ln -f /usr/local/bin/pull-private /etc/cron.daily/pull-private
