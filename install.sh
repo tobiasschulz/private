cfa2e5dd3070e94f8e5b8b31aaf354857eb9b624
Tobias Schulz <tobias@freehal.net>
2013-01-26 18:40:02 +0100

    default system configurations added

--- install.sh
+++ install.sh
@@ -3,12 +3,21 @@
 LC_ALL=C git pull 2>&1 | grep -v "Current branch master is up to date" | grep -v "Already up-to-date"
 
 export P=$(pwd)
+
+# .bashrc
+
 for u in /etc/skel /root /home/*
 do
 	ln -sf $P/home-.bashrc $u/.bashrc
 done
 chown root:root $P/home-.bashrc
 chmod u=rwx,g=rx,o=rx $P/home-.bashrc
+
+# update system config
+
+cp -f $P/apt.conf /etc/apt/apt.conf.d/60user
+
+# setup update script
 
 echo '#!/bin/bash' > /usr/local/bin/pull-private
 echo 'sudo su -c "cd '$P'; ./install.sh"' >> /usr/local/bin/pull-private
